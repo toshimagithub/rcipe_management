3653aa5cc342711aa6101a3cd5a71f7e94215918
toshimagihub <141797646+toshimagithub@users.noreply.github.com>
2023-12-26 11:54:54 +0900

    コメントの高さを修正

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -56,11 +56,12 @@
                 </div>
                 <label>コメント</label>
 
-                <textarea class="takasa w-auto py-1 border border-gray-300 rounded-md font-semibold whitespace-pre-line
+                <textarea class="height w-auto py-1 border border-gray-300 rounded-md font-semibold whitespace-pre-line
                 @error('comment') is-invalid @enderror"
-                name="comment" cols="60" rows="5" style="resize: none; border-radius: 10px; height: 150px;">
-                {{ old('comment', $recipe->comment) }}
+                name="comment" style="resize: none; border-radius: 10px; height: 230px;">
+                    {{ old('comment', $recipe->comment) }}
                 </textarea>
+    
                 @error('comment')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
